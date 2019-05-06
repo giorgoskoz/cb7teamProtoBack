0ee3328b8e1f6a479dd1e0da2165185276594175
staskost <stasinoskostas@outlook.com>
2019-05-06 21:29:59 +0300

    added ability to upload photos

--- src/main/java/com/klicks/klicks/KlicksApplication.java
+++ src/main/java/com/klicks/klicks/KlicksApplication.java
@@ -2,8 +2,12 @@ package com.klicks.klicks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.klicks.klicks.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class KlicksApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/klicks/klicks/validation/Validation.java
+++ src/main/java/com/klicks/klicks/validation/Validation.java
@@ -3,6 +3,8 @@ package com.klicks.klicks.validation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.klicks.klicks.entities.ExtraGear;
+import com.klicks.klicks.entities.StandartGear;
 import com.klicks.klicks.entities.Token;
 import com.klicks.klicks.entities.User;
 
@@ -18,6 +20,18 @@ public class Validation {
 	public static void validateUser(User user) {
 		if (user == null) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found");
+		}
+	}
+	
+	public static void validateStandartgear(StandartGear gear) {
+		if (gear == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Gear Not Found");
+		}
+	}
+	
+	public static void validateExtragear(ExtraGear gear) {
+		if (gear == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Gear Not Found");
 		}
 	}
 
